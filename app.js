60401ae5af08c14d35515bd91d991e4767442276
marsalansohail123 <arsalan,sohail1934@gmail.com>
2022-12-23 15:19:08 +0500

    Work Done

---
+++ app.js
@@ -0,0 +1,29 @@
+// Long Way
+
+// var mainDiv = document.getElementById("main")
+// console.log(main)
+
+// function circle() {
+//     mainDiv.className = 'circle';
+// }
+
+// function rectengle() {
+//     mainDiv.className = 'rectengle';
+// }
+
+// function oval() {
+//     mainDiv.className = 'oval';
+// }
+
+
+
+
+// Short Way and Good Practice
+
+var div = document.getElementById("main");
+
+console.log(main)
+
+function chngShape(p) {
+    div.className = p;
+}
